ed98a95891fb0bbdd5b59d65276f332d52b9454f
berin <berin.hadziabdic@genspark.net>
2022-12-19 12:51:07 -0800

    Adding in styling dependencies.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import MaterialModule from "./modules/material-module";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/modules/material-module.ts
@@ -0,0 +1,7 @@
+import {NgModule} from "@angular/core";
+import {MatButtonModule} from "@angular/material/button";
+
+@NgModule({
+  imports:[MatButtonModule]
+})
+export default class MaterialModule{}
