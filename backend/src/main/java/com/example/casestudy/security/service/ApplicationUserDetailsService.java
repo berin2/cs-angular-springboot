34cfe8f8cec31c111f4dc7e9201faee0379c4840
berin <berin.hadziabdic@genspark.net>
2022-12-21 08:08:13 -0800

    Enabled JPA and set basic properties for h2,hibernate, and etc.

--- backend/src/main/java/com/example/casestudy/security/service/ApplicationUserDetailsService.java
+++ backend/src/main/java/com/example/casestudy/security/service/ApplicationUserDetailsService.java
@@ -5,12 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ApplicationUserDetailsService implements UserDetailsService {
     protected final RApplicationDetails repository;
+    protected final PasswordEncoder encoder;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
